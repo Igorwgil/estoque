c2faad6cdf62f20ad6280112a5b2d87bd1954998
Igor Daniel <igordaniel.wgil6@gmail.com>
2025-05-19 17:03:10 -0400

    Pagina principal e arquivos de funções forma criados

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print('[1] realizar venda\n[2]abastecer estoque\n[3]fechar programa')
-opcao = int(input('Digite uma opcao'))
+from functions.untils import limpar_tela
+
+limpar_tela()
+
+estoque_total = 0
+
+try:
+    opcao = int(input('[1] Repor estoque'))
+except ValueError:
+    print('Valor inválido. Somente números')
+except:
+    print('Erro inesperado.')
+
+'''fazer vendas
+repor estoque
+consultar vendas
+fechar programa
+'''
+
+
+
+
+
+
+
+
